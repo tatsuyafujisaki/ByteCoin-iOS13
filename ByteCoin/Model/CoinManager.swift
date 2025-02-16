b7654e62a65426ce72c07fcf5b362fd33f1fb921
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-02-16 14:58:43 +0900

    Step 168

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,26 +7,40 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
 
     func getCoinPrice(for currency: String) {
-        // Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
 
-        // Use optional binding to unwrap the URL that's created from the urlString
         if let url = URL(string: urlString) {
-            // Create a new URLSession object with default configuration.
             let session = URLSession(configuration: .default)
-
-            // Create a new data task for the URLSession
             let task = session.dataTask(with: url) { data, _, error in
                 if error != nil {
                     print(error!)
                     return
                 }
-                // Format the data we got back as a string to be able to print it.
-                let dataAsString = String(data: data!, encoding: .utf8) ?? ""
-                print(dataAsString)
+
+                if let safeData = data {
+                    let bitcoinPrice = self.parseJSON(safeData)
+                }
             }
-            // Start task to fetch data from bitcoin average's servers.
             task.resume()
+        }
+    }
+
+    func parseJSON(_ data: Data) -> Double? {
+        // Create a JSONDecoder
+        let decoder = JSONDecoder()
+        do {
+            // try to decode the data using the CoinData structure
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+
+            // Get the last property from the decoded data.
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+
+        } catch {
+            // Catch and print any errors.
+            print(error)
+            return nil
         }
     }
 }
